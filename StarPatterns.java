23fe48fc5902b12a0aba854f683a33cd38e24e21
Ben Godfrey <BenJetson@users.noreply.github.com>
2018-01-16 10:21:04 -0500

    finish starZ

--- StarPatterns.java
+++ StarPatterns.java
@@ -56,7 +56,16 @@ class StarPatterns
 	
 	public static void starZ(int h)
 	{
-	
+		int w = h;
+
+		for (int i=0; i<h; i++) {
+			for (int j=0; j<w; j++) {
+				if (i == 0 || i == h-1 || i + j == h - 1) System.out.print("*");
+				else System.out.print(" ");
+				// System.out.print(i + "" + j + " ");
+			}
+			System.out.println();
+		}
 	}
 
 	public static void main(String[] args) 
